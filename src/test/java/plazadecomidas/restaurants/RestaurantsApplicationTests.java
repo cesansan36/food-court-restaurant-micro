e9cedfa6c2bbf8d8cbe41bb6a7ff870397f493d6
Carlos Santarrosa <carlossantarrosa63@gmail.com>
2024-05-19 08:37:58 -0500

    first commit

---
+++ src/test/java/plazadecomidas/restaurants/RestaurantsApplicationTests.java
@@ -0,0 +1,13 @@
+package plazadecomidas.restaurants;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
