270840b2ce636de5bc5e590c7195fd064b6b9f9f
Carlos Santarrosa <carlossantarrosa63@gmail.com>
2024-05-21 21:37:37 -0500

    Added authentication to endpoints

--- src/main/java/plazadecomidas/restaurants/adapters/driving/http/rest/controller/MealControllerAdapter.java
+++ src/main/java/plazadecomidas/restaurants/adapters/driving/http/rest/controller/MealControllerAdapter.java
@@ -3,6 +3,7 @@ package plazadecomidas.restaurants.adapters.driving.http.rest.controller;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -23,6 +24,7 @@ public class MealControllerAdapter {
     private final IMealRequestMapper mealRequestMapper;
 
     @PostMapping("register")
+    @PreAuthorize("hasRole('OWNER')")
     public ResponseEntity<Void> addMeal(@RequestBody AddMealRequest addMealRequest) {
         Meal meal = mealRequestMapper.addMealRequestToMeal(addMealRequest);
         mealServicePort.saveMeal(meal);
@@ -30,6 +32,7 @@ public class MealControllerAdapter {
     }
 
     @PutMapping("update")
+    @PreAuthorize("hasRole('OWNER')")
     public ResponseEntity<Void> updateMeal(@RequestBody UpdateMealRequest updateMealRequest) {
         Meal meal = mealRequestMapper.updateMealRequestToMeal(updateMealRequest);
 
--- src/main/java/plazadecomidas/restaurants/adapters/driving/http/rest/controller/RestaurantControllerAdapter.java
+++ src/main/java/plazadecomidas/restaurants/adapters/driving/http/rest/controller/RestaurantControllerAdapter.java
@@ -3,6 +3,7 @@ package plazadecomidas.restaurants.adapters.driving.http.rest.controller;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,6 +21,7 @@ public class RestaurantControllerAdapter {
     private final IRestaurantRequestMapper restaurantRequestMapper;
 
     @PostMapping("register/restaurant")
+    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<Void> addRestaurant(@RequestBody AddRestaurantRequest addRestaurantRequest) {
 
         restaurantServicePort.saveRestaurant(
